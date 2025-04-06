11b3cece54e5e41fcd8e7fa6d388747d83765aa6
Hetman3 <vlad.paschenko@gmail.com>
2025-04-06 15:21:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,7 @@ def liqpay_callback():
     return jsonify({"status": "ok"}), 200
 
 def run_flask():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+   app.run(debug=True, host="0.0.0.0", port=5000)
 
 if __name__ == '__main__':
     flask_process = Process(target=run_flask)
